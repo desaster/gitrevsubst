44ce0c578c64ceb02625f7f9e5350c09a5bfd7f6
Upi Tamminen <desaster@gmail.com>
2016-02-20 14:49:01 +0200

    initial commit

---
+++ gitrevsubst/Git.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace gitrevsubst
+{
+    public class Git
+    {
+        private string gitDirectory;
+
+        public Git(string gitDirectory)
+        {
+            this.gitDirectory = gitDirectory;
+        }
+
+        public string GetShortRevId()
+        {
+            string rev = this.GetGitOutput(string.Format(
+                "--git-dir {0} rev-parse --short HEAD",
+                this.gitDirectory));
+            if (rev.Length != 7) {
+                Debug.WriteLine("Revision length invalid: [{0}]", rev);
+                return null;
+            }
+            return rev;
+        }
+
+        public string GetLongRevId()
+        {
+            string rev = this.GetGitOutput(string.Format(
+                "--git-dir {0} rev-parse HEAD",
+                this.gitDirectory));
+            if (rev.Length != 40) {
+                Debug.WriteLine(string.Format(
+                    "Revision length invalid: [{0}]", rev));
+                return null;
+            }
+            return rev;
+        }
+
+        public DateTime? GetDate(string rev)
+        {
+            string output = this.GetGitOutput(string.Format(
+                "--git-dir {0} rev-list --format=format:'%ai' --max-count=1 {1}",
+                this.gitDirectory, rev));
+
+            string dateStr = null;
+            using (var rdr = new StringReader(output)) {
+                while (true) {
+                    string l = rdr.ReadLine();
+                    if (l != null) {
+                        if (l.StartsWith("'") && l.EndsWith("'")) {
+                            dateStr = l.Substring(1, l.Length - 2);
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            return (dateStr == null) ? null : (DateTime?) DateTime.Parse(dateStr);
+        }
+
+        private string GetGitOutput(string arguments)
+        {
+            Process p = new Process();
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "git.exe";
+            p.StartInfo.Arguments = arguments;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            if (p.ExitCode != 0) {
+                Debug.WriteLine("Process returned error code " + p.ExitCode);
+                return null;
+            }
+
+            output = output.Trim(); // remove line ending
+
+            return output;
+        }
+    }
+}
---
+++ gitrevsubst/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace gitrevsubst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3) {
+                Console.WriteLine(
+                    "Usage: GITREVSUBST.EXE <GitDirectory> <InputFile> <OutputFile>");
+                Environment.Exit(1);
+            }
+
+            string gitDir = args[0];
+            string inputFile = args[1];
+            string outputFile = args[2];
+
+            var git = new Git(gitDir);
+            string rev = git.GetShortRevId();
+
+            if (rev == null) {
+                Console.WriteLine("Error retrieving git revision!");
+                Environment.Exit(2);
+            }
+
+            DateTime? date = git.GetDate(rev);
+
+            if (date == null) {
+                Console.WriteLine("Error retrieving git commit date!");
+                Environment.Exit(2);
+            }
+
+            string contents = System.IO.File.ReadAllText(inputFile);
+
+            contents = contents.Replace("$GITREV$", rev);
+
+            contents = contents.Replace("$GITDATE$",
+                ((DateTime) date).ToString("yyyyddMM"));
+
+            System.IO.File.WriteAllText(outputFile, contents);
+        }
+    }
+}
